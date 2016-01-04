eef0dcaf1dee5721bff0dd9267f005ed20b7e1c2
Edward-Yao-Zhigang <zhigang_edward@hotmail.com>
2016-01-04 03:32:01 +0000

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'edwardyao1', email: 'edwardyao1@hotmail.com', password: 'sdklsdkl', password_confirmation: 'sdklsdkl')
+User.create(username: 'edwardyao2', email: 'edwardyao2@hotmail.com', password: 'sdklsdkl', password_confirmation: 'sdklsdkl')
+User.create(username: 'edwardyao3', email: 'edwardyao3@hotmail.com', password: 'sdklsdkl', password_confirmation: 'sdklsdkl')
+User.create(username: 'edwardyao4', email: 'edwardyao4@hotmail.com', password: 'sdklsdkl', password_confirmation: 'sdklsdkl')
+User.create(username: 'edwardyao5', email: 'edwardyao5@hotmail.com', password: 'sdklsdkl', password_confirmation: 'sdklsdkl')
+p "Test users created"
