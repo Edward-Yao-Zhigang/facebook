415d020efd61cd459d1625bf63daa2bde653fb04
Edward-Yao-Zhigang <zhigang_edward@hotmail.com>
2016-01-04 16:15:45 +0000

    added pending and requested function

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,16 @@ class User < ActiveRecord::Base
   def request_friendship(user_2)
   	self.friendships.create(friend: user_2)
   end
+
+  def pending_friends
+  	self.inverse_friendships.where(state: "pending")
+  end
+
+  def requested_friends
+  	self.friendships.where(state: "pending")
+  end
+
+  def active_friends
+  	self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:user)
+  end
 end
