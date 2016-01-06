2d16f49f3a87eecb7414214307deef2524eabe4c
Edward-Yao-Zhigang <zhigang_edward@hotmail.com>
2016-01-06 04:56:11 +0000

    added posts function

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,33 @@ class Post < ActiveRecord::Base
 	validates_presence_of :user_id
 	validates_presence_of :content
 
-	auto_html_for :content do
-		html_escape
-		image
-		youtube(width: "100%", height: 250, autoplay: false)
-		link target: "_blank", rel: "nofollow"
-		simple_format
+	def hosting=(str)
+    self[:hosting_html] = format.call(str)
+    super
+  end
+
+  def editor=(str)
+    self[:editor_html] = format.call(str)
+    super
+  end
+
+  private
+
+  def format
+    @@format ||= 
+      Format.new [
+        Link.new(target: "_blank"),
+        SimpleFormat.new,
+        Image.new,
+        Youtube.new(width: "100%", height: 250, autoplay: false)
+      ]
+  end
+
+	# auto_html_for :content do
+	# 	html_escape
+	# 	image
+	# 	youtube(width: "100%", height: 250, autoplay: false)
+	# 	link target: "_blank", rel: "nofollow"
+	# 	simple_format
+	# end
 end
