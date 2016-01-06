d9787ad71c47f63443d98f5a5916183fb5edf625
Edward-Yao-Zhigang <zhigang_edward@hotmail.com>
2016-01-06 23:49:23 +0000

    added timeline

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
       end
     end
     resources :posts, only: [:create, :edit, :update, :destroy]
+    resources :activities, only: [:index]
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
